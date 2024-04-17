d0dcb4ac73c529922e7700f1cfb204819d6c51b0
JulianGonzalez <juliangonnza1234@gmail.com>
2024-04-17 12:17:07 -0500

    PayModeRepositorySearch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Supermarket_mvp._Repositories;
+using Supermarket_mvp.Models;
 using Supermarket_mvp.Views;
+using Supermarket_mvp.Properties;
+using System.Configuration;
+using Supermarket_mvp.Presenters;
 
 namespace Supermarket_mvp
 {
@@ -12,8 +17,13 @@ namespace Supermarket_mvp
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new PayModeView());
+            string sqlConnectionString = "";
+            IPayModeView view = new PayModeView();
+            IPayModeRepository reposity = new PayModeRepository(sqlConnectionString);
+            new PayModePresenter(view, reposity);
+            Application.Run((Form)view);
         }
     }
 }
--- obj/Debug/net8.0-windows/SupermarketMVP.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SupermarketMVP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketMVP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914d0f97fc7af3fcb16461712027be07c9da3393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789b64bc02b5cbe2be8fa203e8e692f14b624ef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketMVP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketMVP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
