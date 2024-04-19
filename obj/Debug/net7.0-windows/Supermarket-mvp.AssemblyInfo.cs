58ab6b985eec008bf064cf3b656f392c4ffe55e3
JulianGonzalez <juliangonnza1234@gmail.com>
2024-04-19 18:15:17 -0500

    Categories Presenter

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1637f7e3924638c6db620f645bbd32f53ecf3c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+268739f500ef0cf60ee6cd92d3f895b65368a642")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
