e86f7bfb5ea62d87aeff232f3b71e6559dffcb11
JulianGonzalez <juliangonnza1234@gmail.com>
2024-04-18 19:43:02 -0500

    Product Repository

---
+++ _Repositories/ProductRespository.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Data.SqlClient;
+using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public void Add(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Product VALUES (@name, @observation, @price)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = productModel.Observation;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM Product WHERE Product_Id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Edit(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE Product
+                                    SET Product_Name =@name,
+                                    Product_Observation = @observation,
+                                    Product_Price =@price
+                                    WHERE Product_Id = @id";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@observation", SqlDbType.NVarChar).Value = productModel.Observation;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productModel.Id;
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<ProductModel> GetAll()
+        {
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Product ORDER BY Product_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["Product_Id"];
+                        productModel.Name = reader["Product_Name"].ToString();
+                        productModel.Observation = reader["Product_Observation"].ToString();
+                        productList.Add(productModel);
+                    }
+                }
+            }
+            return productList;
+        }
+
+        public IEnumerable<ProductModel> GetByValue(string value)
+        {
+            var productList = new List<ProductModel>();
+            int productId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string productName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Product
+                                    WHERE Product_Id=@id or Product_Name LIKE @name+ '%'
+                                    ORDER By Product_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["Product_Id"];
+                        productModel.Name = reader["Product_Name"].ToString();
+                        productModel.Observation = reader["Product_Observation"].ToString();
+                        productList.Add(productModel);
+                    }
+                }
+
+            }
+            return productList;
+
+        }
+    }
+
+}
--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7710b4d8079e6ce72e04f154a3cdecdfaed3254")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87eef11755753c0aac0931046a2b5f5c2a9e36b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
