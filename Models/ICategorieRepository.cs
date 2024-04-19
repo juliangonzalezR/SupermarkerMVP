0b17c71dc9a6adb6dbdc09d9cdc839c551004ec6
JulianGonzalez <juliangonnza1234@gmail.com>
2024-04-19 18:43:38 -0500

    Icategories Repository

---
+++ Models/ICategorieRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface ICategorieRepository
+    {
+
+        void Add(CategorieModel categorieModel);
+        void Edit(CategorieModel categorieModel);
+        void Delete(int id);
+        IEnumerable<CategorieModel> GetAll();
+        IEnumerable<CategorieModel> GetByValue(string value);
+    }
+}
