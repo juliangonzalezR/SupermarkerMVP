2e3c2ee3e12b4ebea87f3b3ce183abb5ef2519dc
JulianGonzalez <juliangonnza1234@gmail.com>
2024-04-19 11:05:44 -0500

    IProvider Repository

--- obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2939dd43269361279e3b7cfb0af192b0ed56a8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbae16f528c01144c6a9f0336c4940870da48360")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
